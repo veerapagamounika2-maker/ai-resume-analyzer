61e7d483c415803cbad4bba843b1b4da76de8d2d
veerapaga mounika <veerapagamounika2@gmail.com>
2026-03-20 14:09:19 +0530

    Remove test files

--- test_parser.py
+++
@@ -1,5 +0,0 @@
-from parser import extract_from_docx
-
-text = extract_from_docx("entry level resume.docx")
-print(text[:500])
-print("Total words:", len(text.split()))
--- test_preprocessor.py
+++
@@ -1,6 +0,0 @@
-from preprocessor import preprocess
-
-sample = "Experienced in Running Machine Learning Models and building Pipelines!!!"
-cleaned = preprocess(sample)
-print("Before:", sample)
-print("After:", cleaned)
